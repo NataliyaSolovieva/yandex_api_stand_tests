9dddf4c41549b50be2932cedca22f6f15c29602a
NataliyaSolovieva <solovievanataliya@gmail.com>
2023-12-12 16:28:06 +0300

    Added new tests

---
+++ configuration.py
@@ -0,0 +1,4 @@
+URL_SERVICE = "https://b363a6f0-f009-4095-befa-357857567545.serverhub.praktikum-services.ru"
+DOC_PATH = "/docs/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
